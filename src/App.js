396c54fdd225951fa95d584ba8de34be616db0e7
gamichwi <gamichwi@gmail.com>
2020-08-06 16:33:01 +1000

    Form Created.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import './App.css';
 
 const App = () => {
   
+  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}`
+
   return (
     <div clasName="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className ="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 }
